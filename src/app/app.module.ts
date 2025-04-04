7ebd80ea920eb3442827f7cca01cbc6a120fa06f
m0usedev / Álvaro <alvaro.sobrino.coding@gmail.com>
2025-04-04 11:15:27 +0200

    add components app and edit css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductAlertsComponent } from './components/product-alerts/product-alerts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertsComponent
   ],
   imports: [
     BrowserModule,
