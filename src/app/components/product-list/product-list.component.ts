62d4f47f9cd20cdab7f6f9a2bbe101ecfba6c4fa
m0usedev / Álvaro <alvaro.sobrino.coding@gmail.com>
2025-04-04 17:23:05 +0200

    product-list

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { products, Product } from 'src/app/products';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-
+  public products : Product[] = products;
 }
