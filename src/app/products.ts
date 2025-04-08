c40b56fa701f3c16a9e18e210dc51f4e88008444
m0usedev <alvaro.sobrino.coding@gmail.com>
2025-04-08 20:38:33 +0200

    add id products

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,19 +1,23 @@
 export interface Product {
+  id: number;
   name: string;
   description?: string;
   price: number;
 }
 export let products : Product[] = [
   {
+    id: 0,
     name : "Phone XL",
     description : "Esto es una descripcion de Phone XL",
     price : 10
   },
   {
+    id: 1,
     name : "Phone Mini",
     price : 300
   },
   {
+    id: 2,
     name : "Phone Standar",
     description : "Esto es una descripcion de Phone Standar",
     price : 800
