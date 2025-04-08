b17b4b789b4f7d99989203072ae4da8fc1e0ed0d
m0usedev <alvaro.sobrino.coding@gmail.com>
2025-04-08 20:39:07 +0200

    change style and add rutes products links

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { products, Product } from 'src/app/products';
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  public products : Product[] = products;
+  public products: Product[] = products;
 
-  share() : void {
+  share(): void {
     window.alert("¡El producto ha sido compartido!");
   }
 
